79b4044e9bf4c56c06b6584c450d6859d9c5c6b7
Pr75502 <pp1614582@gmail.com>
2025-03-10 19:55:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+let form=document.querySelector("form")
+let name=document.querySelector("#username")
+let pass=document.querySelector("#password")
+let checkBox=document.querySelector("#checkbox")
+let submitBtn=document.querySelector("#submit")
+let exitBtn=document.querySelector("#existing")
+
+if(localStorage.getItem("name")!=null){
+		exitBtn.disabled=false;
+	}
+
+form.addEventListener("submit",(e)=>{
+	e.preventDefault()
+	if(name.value!="" && pass.value!=""){
+		alert(`Logged in as ${name.value}`)
+	}
+	if(checkBox.checked){
+		localStorage.setItem("name",name.value)
+		localStorage.setItem("pass",pass.value)
+	}else{
+		localStorage.removeItem("name")
+		localStorage.removeItem("pass")
+	}
+	if(localStorage.getItem("name")!=null){
+		exitBtn.disabled=false;
+	}
+	
+})
+
+exitBtn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let storedName=localStorage.getItem("name")
+	if(storedName){
+		alert(`Logged in as ${localStorage.getItem("name")}`)
+	}else{
+		alert(`no user found`)
+	}
+})
